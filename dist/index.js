1fabb1bd396a9050a5e42e90273f36cc7088d7db
GrantBirki <grantbirki@github.com>
2023-01-26 11:18:23 -0700

    init

---
+++ dist/index.js
@@ -0,0 +1,57 @@
+require('./sourcemap-register.js');/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	// The require scope
+/******/ 	var __nccwpck_require__ = {};
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__nccwpck_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__nccwpck_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+__nccwpck_require__.r(__webpack_exports__);
+/* harmony export */ __nccwpck_require__.d(__webpack_exports__, {
+/* harmony export */   "run": () => (/* binding */ run)
+/* harmony export */ });
+// import * as core from '@actions/core'
+// import * as github from '@actions/github'
+// import {context} from '@actions/github'
+
+// :returns: 'success', 'success - noop', 'failure', 'safe-exit', or raises an error
+async function run() {}
+
+run()
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
+//# sourceMappingURL=index.js.map
---
+++ src/main.js
@@ -0,0 +1,8 @@
+// import * as core from '@actions/core'
+// import * as github from '@actions/github'
+// import {context} from '@actions/github'
+
+// :returns: 'success', 'success - noop', 'failure', 'safe-exit', or raises an error
+export async function run() {}
+
+run()
